c965f56bcd3014f69b713432df19af771a4334a2
Ruby <pineapples1104@gmail.com>
2024-04-24 10:01:53 -0600

    retry on adding c# notation

--- sandbox/Nemo/Program.cs
+++ sandbox/Nemo/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// to get input
+string x;
+Console.Write('prompt');
+x = Console.ReadLine();
+
+// integers, float, Boolean variables
+int i;
+double f;
+bool b;
+
+// output/display
+Console.WriteLine('')
+
+//Opereators
++
+-
+*
+/
+%
+
+//Logic
+&& = and
+|| = or
+!  = not
+
+//control structures
+// if (test) {block of code}
+// else if
+// else
+    
+}
