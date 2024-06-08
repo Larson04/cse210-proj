6894ad29a1ba3efd4462e6acee1f106a116ab7e9
Ruby <pineapples1104@gmail.com>
2024-06-08 16:54:07 -0600

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Carlos", "Fractions", "Section 7.3", "8-19");
+
+        Console.WriteLine(mathAssignment.GetSummary());
+
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Eunice Anning", "European History", "The Causes of World War II");
+
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
