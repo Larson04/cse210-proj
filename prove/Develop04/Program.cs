45ad8c49fa6185aa3e7a1aeeec512cb845b947dd
Ruby <pineapples1104@gmail.com>
2024-06-13 12:42:30 -0600

    mindfulness mod

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -15,15 +15,15 @@ class Program
 
     
 
-        // Breath breathActivity = new Breath("Breathe in...", "Breathe out...", activity._duration, 4, "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+        Breath breathActivity = new Breath("Breathe in...", "Breathe out...", activity._duration, 4, "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
 
-        // breathActivity.BreathCount(4, 15, "Great Job!");
+        breathActivity.BreathCount(4, 15, "Great Job!");
 
-        Listing listingActivity = new Listing("Welcome to the Mindfulness App! Here are some activities to help you be more mindful.", "Great Job!", activity._duration, "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area." );
+        // Listing listingActivity = new Listing("Welcome to the Mindfulness App! Here are some activities to help you be more mindful.", "Great Job!", activity._duration, "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area." );
 
-        listingActivity.DisplayPrompt();
-        listingActivity.GetResponse(activity._duration);
-        listingActivity.Count();
+        // listingActivity.DisplayPrompt();
+        // listingActivity.GetResponse(activity._duration);
+        // listingActivity.Count();
 
     }
 }
